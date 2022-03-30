9e70feccb880e1d94c9bd181c6af20638d6bfc92
michael-duy <90901739+michael-duy@users.noreply.github.com>
2022-03-30 22:53:52 +0700

    5-jwt-2

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,7 +1,24 @@
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
+const CustomAPIError = require("../errors/custom-error");
 
 const login = async (req, res) => {
-    res.send("Fake Login/Register/Signup Route");
+    const {username, password} = req.body;
+
+    if (!username || !password) {
+        throw new CustomAPIError("Please provide email and password", 400);
+    }
+
+    const id = new Date().getDate();
+
+    const token = jwt.sign({
+        id,
+        username
+    }, process.env.JWT_SECRET, { expiresIn: "30d" });
+
+    res.status(200).json({
+        msg: "user created",
+        token
+    });
 };
 
 const dashboard = async (req, res) => {
